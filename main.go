9d7ab9587824a4b61b3fce6154822906cf9b73e8
Alfiankan <git@alfiankan.com>
2021-01-01 09:37:51 +0700

    cleaning debug

--- main.go
+++ main.go
@@ -1,23 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"teleterm/handler"
 )
 
-func eerr(e error) {
-	if e != nil {
-		fmt.Println(e)
-	}
-}
 func main() {
 	handler.Begin()
-	//result,er := exec.Command("cat","kkk.txt").Output()
-	//fmt.Println(string(result))
-	//fmt.Println(len(string(result)))
-	//fmt.Println(er)
-	//fmt.Println("pol")
-	//d1 := result
-	//err := ioutil.WriteFile("/tmp/log.txt", d1, 0644)
-	//eerr(err)
 }
